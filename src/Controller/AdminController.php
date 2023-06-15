fbbeeeadac28b7db57ccc61df6a65976f3dc07d4
Zeenath-Soekha <302458451@student.rocmondriaan.nl>
2023-06-15 23:55:47 +0200

    refactor: add use statement for user edit type

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 use App\Entity\User;
+use App\Form\UserEditType;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
