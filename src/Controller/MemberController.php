3557e1348da9189326d01d509851190d5567b595
Zeenath-Soekha <302458451@student.rocmondriaan.nl>
2023-06-15 10:25:20 +0200

    feat: made profile

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -1,18 +1,29 @@
 <?php
+    namespace App\Controller;
+    use App\Entity\User;
+    use App\Repository\RegistrationRepository;
+    use App\Repository\UserRepository;
+    use App\Form\UserType;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Bundle\SecurityBundle\Security;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
 
-namespace App\Controller;
+    class MemberController extends AbstractController
+    {
+        public function __construct(private readonly Security $security, private readonly RegistrationRepository $registrationRepository, private readonly UserRepository $userRepository) {}
 
-use App\Form\UserType;
-use Psr\Container\ContainerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+        #[Route('/profile', name: 'app_profile')]
+        public function index(UserRepository $userRepository): Response
+        {
 
-class MemberController extends AbstractController
-{
-    public function __construct(private readonly Security $security) {}
+            $user = $this->getUser();
+            return $this->render('member/index.html.twig', [
+                'controller_name' => 'MemberController',
+                'user' => $user,
+            ]);
+        }
 
     #[Route('/member', name: 'app_member')]
     public function index(): Response
