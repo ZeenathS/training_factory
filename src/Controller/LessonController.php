275a0e6c4d81451d1e99e8552996dc793b79145f
Zeenath-Soekha <302458451@student.rocmondriaan.nl>
2023-06-15 09:21:01 +0200

    feat: unregister function added

--- src/Controller/LessonController.php
+++ src/Controller/LessonController.php
@@ -9,15 +9,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class LessonController extends AbstractController
 {
     #[Route('/lesson/{lesson}/register', name: 'app_lesson_register')]
+    #[IsGranted('IS_AUTHENTICATED')]
     public function registerForLesson(Lesson $lesson, Security $security, RegistrationRepository $registrationRepository): Response
     {
         // If user is not already registered to the lesson.
         if(
-            !$lesson->getRegistrations()->exists(function(Registration $registration) use($security) {
+            !$lesson->getRegistrations()->exists(function($_, Registration $registration) use($security) {
                 return $registration->getUser() === $security->getUser();
             })
         ) {
@@ -29,6 +31,24 @@ class LessonController extends AbstractController
             $registrationRepository->save($registration, true);
         }
 
-        return $this->redirectToRoute('app_training_lessons', ['training', $lesson->getTraining()->getId()]);
+        return $this->redirectToRoute('app_training_lessons', ['training' => $lesson->getTraining()->getId()]);
+    }
+
+    #[Route('/lesson/{lesson}/unregister', name: 'app_lesson_unregister')]
+    #[IsGranted('IS_AUTHENTICATED')]
+    public function unregisterForLesson(Lesson $lesson, Security $security, RegistrationRepository $registrationRepository): Response
+    {
+        if(
+            $lesson->getRegistrations()->exists(function($_, Registration $registration) use($security) {
+                return $registration->getUser() === $security->getUser();
+            })
+        ) {
+            $registration = $lesson->getRegistrations()->findFirst(function($_, Registration $registration) use($security) {
+                return $registration->getUser() === $security->getUser();
+            });
+            $registrationRepository->remove($registration, true);
+        }
+
+        return $this->redirectToRoute('app_training_lessons', ['training' => $lesson->getTraining()->getId()]);
     }
 }
